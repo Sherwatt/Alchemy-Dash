25d5582bf63ec5268305f5a249eb5f37360a2a16
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-23 15:46:27 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,20 +4,23 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.preload,spritesheet('player', './assets/test_char.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 2});
+        this.load.spritesheet('player', './assets/test_char.png', {frameWidth: 60, frameHeight: 62, startFrame: 0, endFrame: 3});
     }
     create() {
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        
-        this.player = new Alchemist(this, 100, 20, 'player').setOrigin(0.5, 0);
+
+        this.player = new Alchemist(this, 250, 200, 'player').setOrigin(0.5, 0);
 
         this.anims.create({
             key: 'runner',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2, first: 0}),
-            frameRate: 20
+            frames: this.anims.generateFrameNumbers('player', { frames: [0, 1, 2, 1]}),
+            frameRate: 4,
+            repeat: -1,
         });
+        this.player.play("runner");
+
     }
 }
