279e5f762d4e56687207bf797ec683120909dfb3
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-22 21:13:21 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,9 +7,12 @@ class Play extends Phaser.Scene {
         this.preload,spritesheet('player', './assets/test_char.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 2});
     }
     create() {
+        this.player = new Alchemist(this, 100, 20, 'player').setOrigin(0.5, 0);
+        
         this.anims.create({
-            key: 'player',
+            key: 'runner',
             frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2, first: 0}),
+            frameRate: 20
         });
     }
 }
