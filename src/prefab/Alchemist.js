57388fac5a510e99f58a4157fb76d0d6c4de2eb4
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-22 20:49:21 -0700

    Create Alchemist.js

---
+++ src/prefab/Alchemist.js
@@ -0,0 +1,8 @@
+const { Phaser } = require("../../lib/phaser");
+
+class Alchemist extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+    }
+}
