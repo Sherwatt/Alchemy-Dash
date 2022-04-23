89451ba645abfbb7df42b551909ecea82901ee46
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-22 20:49:27 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,4 +6,10 @@ class Play extends Phaser.Scene {
     preload() {
         this.preload,spritesheet('player', './assets/test_char.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 2});
     }
+    create() {
+        this.anims.create({
+            key: 'player',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2, first: 0}),
+        });
+    }
 }
