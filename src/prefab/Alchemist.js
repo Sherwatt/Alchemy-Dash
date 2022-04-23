72ac74fcf1d4cc4f35512b8fdf9e0740dacd7e8e
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-23 16:03:50 -0700

    Update Alchemist.js

--- src/prefab/Alchemist.js
+++ src/prefab/Alchemist.js
@@ -2,8 +2,14 @@ class Alchemist extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
+        this.moveSpeed = 2;
     }
-    //preload() {
-        //this.preload,spritesheet('player', './assets/test_char.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 2});
-    //}
+
+    update() {
+        if(keyUP.isDown && this.y >= borderUISize) {
+            this.y -= this.moveSpeed;
+        } else if (keyDOWN.isDown && this.y <= game.config.height - borderUISize - this.height){
+            this.y += this.moveSpeed;
+        }
+    }
 }
