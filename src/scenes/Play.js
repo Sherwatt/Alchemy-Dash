b0c8545dd344e959b86cb41b28fd4ba90731fc9b
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-25 12:06:31 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,18 +4,38 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('platform', './assets/brick_tiles_1.png');
         this.load.spritesheet('player', './assets/basic_run_cycle.png', {frameWidth: 64, frameHeight: 96, startFrame: 0, endFrame: 7});
     }
     create() {
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        //adds in a group that'll respresent the ground and any possible platforms we might add
+        this.groundGroup = this.add.group({
+            //this adds all removed platforms to a pool
+            removeCallback: function(platform){
+                platform.scene.groundPool.add(platform)
+            }
+        });
+
+        //here's the pool
+        this.groundPool = this.add.group({
+            //when a platform is removed from the pool it goes into the active group
+            removeCallback: function(platform){
+                platform.scene.groundGroup.add(platform)
+            }
+        });
 
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
-        this.player = new Alchemist(this, 150, 200, 'player').setOrigin(0, 0);
+        this.ground = this.physics.add.staticGroup();
+
+        this.player = this.physics.add.sprite(100, 340, 'player');
+        this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.ground);
+
+        //this.player = new Alchemist(this, 150, 200, 'player').setOrigin(0, 0);
+        //not sure what to do about the Alchemist class now, but we can implement physics, which is good
 
         this.anims.create({
             key: 'runner',
@@ -24,6 +44,8 @@ class Play extends Phaser.Scene {
             repeat: -1,
         });
         this.player.play("runner");
+
+        this.ground.create(100, 400, 'platform');
 
         this.distance = 0;
 
