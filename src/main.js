d9b15d4e6f7669a9f72233789b5d9607192f2059
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-05-04 01:14:50 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 let gameOptions = {
     platformStartSpeed: 350,
-<<<<<<< Updated upstream
-    spawnRange: [100, 350],
-=======
     ingredientStartSpeed: 350,
     spawnRange: [100, 350],
     ingredientSpawnRange: [80, 500],
->>>>>>> Stashed changes
     platformSizeRange: [75, 250],
     playerGravity: 900,
     jumpForce: 400,
@@ -15,11 +11,7 @@ let gameOptions = {
 }
 
 let config = {
-<<<<<<< Updated upstream
-    type: Phaser.AUTO,
-=======
     type: Phaser.CANVAS,
->>>>>>> Stashed changes
     width: 750,
     height: 500,
     autoCenter: true,
